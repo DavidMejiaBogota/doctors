1735adbdc3dd1fd65d5efd772a95099bd70e512a
David Mejia <jose.david.mejia.meneses@gmail.com>
2023-11-24 17:37:22 -0500

    Se crea la función traer paciente por id en el controlador paciente.controllres

--- src/controller/paciente.controller.ts
+++ src/controller/paciente.controller.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "express";
 import {Paciente} from "../models/paciente.model";
 
+//Para traer todos los registros(pacientes)
 export const getPacientes: RequestHandler = async (req, res) => {
     try{
         const pacientes = await Paciente.findAll();
@@ -13,6 +14,29 @@ export const getPacientes: RequestHandler = async (req, res) => {
         res.status(500).json({
             messaage: 'Error al obtener los pacientes',
             error: err.message
+        })
+    }
+}
+//Para traer paciente por id
+export const getPacientesByid: RequestHandler = async (req, res) => {
+    try{
+        const paciente = await Paciente.findByPk(req.params.id);
+        if (paciente) {
+            res.status(200).json({
+                message: 'Operación exitosa',
+                data: paciente
+            });
+        } else {
+            res.status(404).json({
+                message: 'Paciente no encontrado',
+                data: null
+            })
+        }
+    }catch (error:any) {
+        const err = error as Error;
+        res.status(500).json({
+            messaage: 'Error al obtener los pacientes',
+            error: error.message
         })
     }
 }
