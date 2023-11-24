915ad04526a498481e996b9299a52ec0d5dbf006
David Mejia <jose.david.mejia.meneses@gmail.com>
2023-11-24 15:34:26 -0500

    Se crea el model de doctores

---
+++ src/models/doctores.model.ts
@@ -0,0 +1,46 @@
+import { timeStamp } from "console";
+import { Table, Column, Model, DataType, HasMany } from "sequelize-typescript";
+
+@Table({
+    timestamps: false,
+    tableName: 'doctor'
+})
+export class Doctor extends Model {
+
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false,
+        primaryKey: true
+    })
+    id_profesional!: number;
+    
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    nombre!: string;
+    
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    apellido!: string;
+    
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    }) 
+    correo!: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    telefono!: string;
+
+    @Column({
+        type: DataType.ENUM('medicina_interna','medicina_general'),
+        allowNull: false,
+    })
+    especialidad!: string;
+}
